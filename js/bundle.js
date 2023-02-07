b06d0f8d2638ebcbe4c147b96dd7f11ce222eaa5
Max Malevich <malevich.m.439-1@e.tusur.ru>
2023-02-08 00:25:04 +0700

    add TestTask and Grd

---
+++ js/bundle.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./js/script.js ***!
+  \**********************/
+
+
+window.addEventListener('DOMContentLoaded', function() {
+    
+}); 
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
