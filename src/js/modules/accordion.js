987398bea03a63defe17f1f16d46ab88e6d5d471
Max Malevich <malevich.m.439-1@e.tusur.ru>
2023-02-17 10:39:07 +0700

    added edits according to the review
    removed extra classes, added wrapper for accordions, removed unnecessary logic from js

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,23 +1,16 @@
 function accordion(headerSelector, bodySelector){
     let accordionHeader = document.querySelector(headerSelector),
-        accordionBody = document.querySelector(bodySelector);
+        accordionBody = document.querySelector(bodySelector),
+        accordion = document.querySelector('.UI__accordion-wrapper');
 
     accordionHeader.addEventListener('click', () => {
-        accordionBody.classList.toggle("active_accordion_body");
-        accordionHeader.classList.toggle("active_accordion_header");
-        if (accordionBody.classList.contains("active_accordion_body")) {
+        accordion.classList.toggle("accordion_active");
+        if (accordion.classList.contains("accordion_active")) {
             accordionBody.style.maxHeight = accordionBody.scrollHeight + "px";
         } else {
             accordionBody.style.maxHeight = 0;
         }
     }); 
-
-    accordionHeader.addEventListener('mouseover', () => {
-        accordionBody.classList.add('accordion_hover');
-    });
-    accordionHeader.addEventListener('mouseout', () => {
-        accordionBody.classList.remove('accordion_hover');
-    });  
 }
 
 export default accordion;
