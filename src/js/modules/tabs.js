a82e0f63f9cbe2524ba9e1ac8ec3e76d07a8c18f
Max Malevich <malevich.m.439-1@e.tusur.ru>
2023-02-20 09:21:57 +0700

    fixed tabs
    removed an unnecessary function

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,53 +1,55 @@
 function tabs() {
 	const tabs = document.querySelectorAll('.typography__tabs-item');
 	const tabsContent = document.querySelectorAll('.typography__styles');
-    const windowInnerWidth = window.innerWidth;
 
     function showTabFor(device){
         tabs.forEach((item) => {
-            if (item.getAttribute('data-device') === device) 
-                item.classList.add('typography__tabs-item-active')            
+            if (item.dataset.device === device) 
+            {
+                item.classList.add('typography__tabs-item-active')
+            }
             else
+            {
                 item.classList.remove('typography__tabs-item-active')
+            }
+        });
+
+        tabsContent.forEach((item) => {
+            if (item.dataset.device === device) 
+            {
+                item.classList.add('styles_show')
+            }
+            else
+            {
+                item.classList.remove('styles_show')
+            }
         });
     }
-
-    function outputContent(){
-        tabsContent.forEach(item => {
-            item.classList.remove('styles_show');
-        });
-        tabs.forEach((item) => {
-            if (item.classList.contains('typography__tabs-item-active'))
-                tabsContent.forEach(itemContent => {
-                    if (itemContent.getAttribute('data-device') === item.getAttribute('data-device')) 
-                    {
-                        itemContent.classList.add('styles_show');
-                    }else{
-                        itemContent.classList.remove('styles_show');
-                    }
-            });
-        });
-    }
-
-    if (windowInnerWidth < 450)
+ 
+    if (window.innerWidth < 450)
+    {
         showTabFor('mobile');
+    }
     else
+    {
         showTabFor('desktop');
-    outputContent();
+    }
+        
 
     window.addEventListener('resize', (e) => {
         if (e.target.innerWidth < 450)
+        {
             showTabFor('mobile');
+        }            
         else
+        {
             showTabFor('desktop');
-
-        outputContent();
+        }
     });        
 
     tabs.forEach((item) => {
         item.addEventListener('click', (e) => {
-            showTabFor(e.target.getAttribute('data-device'));
-            outputContent();
+            showTabFor(e.target.dataset.device);
         });
     })
 }
